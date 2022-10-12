3a1acc3e8e9c8c3bf3a8c0caea87ea7b6d588aca
Chase Bemis <chase@godwindirect.com>
2022-10-12 15:33:42 -0400

    Section Layout Implementation

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,42 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 observer.observe(pageTop);
+
+//query selector for qa__header-item class
+const qaHeaderItem = document.querySelectorAll(".qa__header-item");
+//loop through the qaHeaderItem array assigning a click event to each element
+qaHeaderItem.forEach((item) => {
+  item.addEventListener("click", function () {
+    event.preventDefault();
+    //if the clicked element has the class active return
+    if (this.classList.contains("active")) {
+      return;
+    } else {
+      //remove the active class from all elements
+      qaHeaderItem.forEach((item) => {
+        item.classList.remove("active");
+      });
+      //add the active class to the clicked element
+      this.classList.add("active");
+      //get the data attribute value of the clicked element
+      const data = this.getAttribute("data-qa");
+      //query selector for qa__content-item class
+      const qaContentItem = document.querySelectorAll(".qa__content-item");
+      //toggle the active class on the qa__content-item element with the same data attribute value as the clicked element
+      qaContentItem.forEach((item) => {
+        if (item.getAttribute("data-qa") === data) {
+          item.classList.toggle("active");
+          //time delay to allow the animation to complete
+          setTimeout(() => {
+            item.classList.toggle("show");
+          }, 100);
+        } else {
+          setTimeout(() => {
+            item.classList.remove("show");
+          }, 100);
+          item.classList.remove("active");
+        }
+      });
+    }
+  });
+});
